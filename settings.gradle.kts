9352f793e008b4d08a833b213bc8d30abe608d13
szhangbiao <szhangbiao@gmail.com>
2026-03-28 09:25:20 +0800

    First commit

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AI Assistant"
+include(":app")
+include(":speech")
+include(":processor")
+include(":downloader")
+include(":common")
