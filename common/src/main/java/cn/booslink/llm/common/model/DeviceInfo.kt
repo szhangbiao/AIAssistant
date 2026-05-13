8e23326f579d9aeef8652cfe0dcfeac669a170f4
szhangbiao <szhangbiao@gmail.com>
2026-05-13 17:45:14 +0800

    Fix auto record audio method

--- common/src/main/java/cn/booslink/llm/common/model/DeviceInfo.kt
+++ common/src/main/java/cn/booslink/llm/common/model/DeviceInfo.kt
@@ -5,6 +5,6 @@ import cn.booslink.llm.common.model.enums.Channel
 data class DeviceInfo(val isDevMode: Boolean, val isSystemApp: Boolean, val channel: Channel) {
 
     fun isAutoAudioRecord(): Boolean {
-        return channel == Channel.VOICE
+        return channel != Channel.VOICE
     }
 }
